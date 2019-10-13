1ead197dcc9be326c6eb69ca715b585a3080bd9f
Dirk Sierd de Vries <dirksierd@me.com>
2019-10-14 01:39:00 +0200

    bugfixes

--- lib/powerpoint/slide/intro.rb
+++ lib/powerpoint/slide/intro.rb
@@ -23,7 +23,7 @@ module Powerpoint
       end
 
       def save_rel_xml(extract_path, index)
-        render_view('textual_rel.xml.erb', "#{extract_path}/ppt/slides/_rels/slide#{index}.xml.rels", index: index)
+        render_view('intro_rel.xml.erb', "#{extract_path}/ppt/slides/_rels/slide#{index}.xml.rels", index: index)
       end
       private :save_rel_xml
 
